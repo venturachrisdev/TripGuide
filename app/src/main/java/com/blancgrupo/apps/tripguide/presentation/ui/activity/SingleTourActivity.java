008f7d9c55306a4eb93061998944aec6e98acef3
venturachrisdev <venturachrisdev@gmail.com>
2017-09-11 16:44:02 -0400

    day finished

--- app/src/main/java/com/blancgrupo/apps/tripguide/presentation/ui/activity/SingleTourActivity.java
+++ app/src/main/java/com/blancgrupo/apps/tripguide/presentation/ui/activity/SingleTourActivity.java
@@ -32,6 +32,7 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SingleTourActivity extends AppCompatActivity
     implements LifecycleRegistryOwner {
@@ -50,6 +51,7 @@ public class SingleTourActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_tour);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
